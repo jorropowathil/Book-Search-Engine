0e2333b9b431407c9c6c3af8ec02df83846ca7b9
jorropowathil <85700575+jorropowathil@users.noreply.github.com>
2021-08-29 22:17:54 -0400

    added schemas folder

--- Develop/server/utils/auth.js
+++ Develop/server/utils/auth.js
@@ -6,7 +6,7 @@ const expiration = '2h';
 
 module.exports = {
   // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
+  authMiddleware: function ({req, res, next}) {
     // allows token to be sent via  req.query or headers
     let token = req.query.token || req.headers.authorization;
 
@@ -33,7 +33,6 @@ module.exports = {
   },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
-
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
