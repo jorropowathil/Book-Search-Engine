a45c14fefd888645d4a584db24121df59c590407
jorropowathil <85700575+jorropowathil@users.noreply.github.com>
2021-09-10 02:51:23 -0500

    more edits to resolvers

--- server/Schemas/resolvers.js
+++ server/Schemas/resolvers.js
@@ -1,4 +1,9 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Thought } = require('../models');
+const { User, Book } = require('../models');
 const { signToken } = require('../utils/auth');
 
+const resolvers = {
+
+}
+
+module.exports = resolvers;
